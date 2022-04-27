bd29cb619b6ef7281b56f2c7857f7c1266a40664
Alexander Morozov <reverse43@gmail.com>
2022-04-27 22:11:55 +0300

    Optimize

--- Алгоритмы - теория и практика. Методы/4. Жадные алгоритмы - теория и задачи/4.1. Введение/2.py
+++ Алгоритмы - теория и практика. Методы/4. Жадные алгоритмы - теория и задачи/4.1. Введение/2.py
@@ -1,17 +1,14 @@
 n_items, capacity = [int(x) for x in input().split()]
-cost_and_volume = [list(map(int, input().split())) for _ in range(n_items)]
+cost_and_volume = [tuple(map(int, input().split())) for _ in range(n_items)]
+cost_and_volume.sort(key=lambda x: -(x[0] / x[1]))
 backpack = {"capacity": capacity, "cost": 0}
 
-for item_cost, item_volume in sorted(cost_and_volume, key=lambda x: -(x[0] / x[1])):
+for item_cost, item_volume in cost_and_volume:
     if item_volume <= backpack["capacity"]:
         backpack["capacity"] -= item_volume
         backpack["cost"] += item_cost
     else:
-        item_piece_multiplier = backpack["capacity"] / item_volume
-        item_piece_weight = item_volume * item_piece_multiplier
-        item_piece_cost = item_cost * item_piece_multiplier
-        backpack["capacity"] -= item_piece_weight
-        backpack["cost"] += item_piece_cost
+        backpack["cost"] += item_cost * (backpack["capacity"] / item_volume)
         break
 
 print(f"{backpack['cost']:.3f}")
